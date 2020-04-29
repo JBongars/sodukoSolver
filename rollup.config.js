3dccbee8b1fe8e6c609a9411476035bab98c4f25
Julien Bongars <julienbongars23@gmail.com>
2020-04-30 03:49:08 +0800

    chore: enhanced rollup functionality

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,28 @@
-export default {
-  input: './index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'iife',
-  }
-}
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import {
+  uglify
+} from 'rollup-plugin-uglify';
+
+export default [{
+    input: './index.js',
+    output: {
+      file: 'dist/index.js',
+      format: 'iife',
+    }
+  },
+  {
+    input: './index.js',
+    output: {
+      file: 'dist/index.min.js',
+      format: 'iife',
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/** '
+      }),
+      commonjs(),
+      uglify()
+    ]
+  },
+]
